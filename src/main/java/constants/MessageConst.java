6faf39f7aa02670df7854de8f9ffb48a9dfe5304
MIKA <MIKA@localhost>
2022-06-22 19:29:11 +0900

    定数ファイルを作成

---
+++ src/main/java/constants/MessageConst.java
@@ -0,0 +1,30 @@
+package constants;
+
+public enum MessageConst {
+
+    I_LOGINED("ログインしました"),
+    E_LOGINED("ログインに失敗しました"),
+    I_LOGOUT("ログアウトしました"),
+
+    I_RESISTERED("登録が完了しました。"),
+    I_UPDATED("更新が完了しました。"),
+    I_DELETED("削除が完了しました。"),
+
+    E_NONAME(""),
+    E_NOPASSWORD(""),
+    E_NOEMP_CODE(""),
+    E_EMP_CODE_EXIST(""),
+    E_NOTITLE(""),
+    E_NOCONTEMT(""),
+
+    private final String text;
+
+    private MessageConst(final String text) {
+        this.text = text;
+    }
+
+    public String getMessage() {
+        return this.text;
+    }
+
+}
