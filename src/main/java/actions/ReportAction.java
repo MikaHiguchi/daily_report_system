8b97f88ca2148cc6150174b9fbefb09ea3f4624c
MIKA <MIKA@localhost>
2022-06-30 03:26:28 +0900

    日報newアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -56,4 +56,20 @@ public class ReportAction extends ActionBase {
         forward(ForwardConst.FW_REP_NEW);
     }
 
+    public void show() throws ServletException, IOException {
+
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        if (rv == null) {
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+            putRequestScope(AttributeConst.REPORT, rv);
+
+            forward(ForwardConst.FW_REP_SHOW);
+
+
+        }
+    }
+
 }
