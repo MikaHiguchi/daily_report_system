332ac470b221328aa900407a34beaa48d0152489
MIKA <MIKA@localhost>
2022-07-23 17:39:12 +0900

    訂正2

--- src/main/java/constants/MessageConst.java
+++ src/main/java/constants/MessageConst.java
@@ -10,12 +10,12 @@ public enum MessageConst {
     I_UPDATED("更新が完了しました。"),
     I_DELETED("削除が完了しました。"),
 
-    E_NONAME(""),
-    E_NOPASSWORD(""),
-    E_NOEMP_CODE(""),
-    E_EMP_CODE_EXIST(""),
-    E_NOTITLE(""),
-    E_NOCONTEMT("");
+    E_NONAME("氏名を入力してください。"),
+    E_NOPASSWORD("パスワードを入力してください。"),
+    E_NOEMP_CODE("社員番号を入力してください。"),
+    E_EMP_CODE_EXIST("入力された社員番号の情報は既に存在しています。"),
+    E_NOTITLE("タイトルを入力してください。"),
+    E_NOCONTEMT("内容を入力してください。");
 
     private final String text;
 
